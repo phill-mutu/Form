d17bc6604b77bbda12a9be707231fe7531a4fe55
Laughton-Mutu <phill.laughton-mutu@capgemini.com>
2023-01-07 14:40:20 +1300

    add seed data

--- server/db/seeds/students.js
+++ server/db/seeds/students.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('students').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('students').insert([
+        {id:1, name: 'Abby', surname: 'Abberforth', email:'abby@email.com', dob: '01/01/2020', username: 'userAbby', password: 'passwordAbby', age:3},
+        {id:2, name: 'Brian', surname: 'Bryant', email:'brian@email.com', dob: '01/01/2021', username: 'userBrian', password: 'passwordBrian', age:2},
+        {id:2, name: 'Charlie', surname: 'Charleston', email:'charlie@email.com', dob: '01/01/2022', username: 'userCharlie', password: 'passwordCharlie', age:1},
       ]);
     });
 };
