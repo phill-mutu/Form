68e0308661aa839ad7304ce504c17e84426ed169
Laughton-Mutu <phill.laughton-mutu@capgemini.com>
2023-01-07 14:54:18 +1300

    set up router

--- server/db/seeds/students.js
+++ server/db/seeds/students.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('students').insert([
-        {id:1, name: 'Abby', surname: 'Abberforth', email:'abby@email.com', dob: '01/01/2020', username: 'userAbby', password: 'passwordAbby', age:3},
-        {id:2, name: 'Brian', surname: 'Bryant', email:'brian@email.com', dob: '01/01/2021', username: 'userBrian', password: 'passwordBrian', age:2},
-        {id:2, name: 'Charlie', surname: 'Charleston', email:'charlie@email.com', dob: '01/01/2022', username: 'userCharlie', password: 'passwordCharlie', age:1},
+        {id:1, name: 'Abby', surname: 'Abberforth', email:'abby@email.com', dob: '01/01/2020', username: 'userAbby', password: 'passwordAbby', age: 3},
+        {id:2, name: 'Brian', surname: 'Bryant', email:'brian@email.com', dob: '01/01/2021', username: 'userBrian', password: 'passwordBrian', age: 2},
+        {id:3, name: 'Charlie', surname: 'Charleston', email:'charlie@email.com', dob: '01/01/2022', username: 'userCharlie', password: 'passwordCharlie', age: 1},
       ]);
     });
 };
--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const path = require('path')
 const express = require('express')
 
+const studentRoutes = require('./routes/students')
+
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
+
+server.use('/api/v1/students', studentRoutes)
 
 module.exports = server
